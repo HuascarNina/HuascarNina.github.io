3b0304fd438961c14ae6b2c08d580ecf42c5e874
Huascar Nina <huascarnina@gmail.com>
2021-06-07 21:31:31 -0400

    Php no envia correo

---
+++ enviar.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['enviar'])) {
+
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$telefono = $_POST['telefono'];
+	$mensaje = $_POST['mensaje'];
+	$header = "From: noreply@example.com" . "\r\n";
+	$header.= "Reply-To: noreply@example.com" . "\r\n";
+	$header.= "X-Mailer: PHP/" . phpversion();
+	$mail = @mail($correo, $telefono, $mensaje, $header);
+	if ($mail) {
+		echo "<h4>Enviado exitosamente</h4>";
+	}
+}
+?>
